1c1ee3ec44429dbef8927c22c32cc663246428da
Jury Soldatenkov <yurysold@gmail.com>
2022-05-29 15:26:58 +0300

    Allow state presevation for compatible random implementation

--- src/Quarks.Tests/RandomExtenstionTests.cs
+++ src/Quarks.Tests/RandomExtenstionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Codestellation.Quarks.Tests
@@ -12,6 +13,7 @@ namespace Codestellation.Quarks.Tests
             const int n = 50;
 
             var original = new Random(123);
+
             for (var i = 0; i < 50; i++)
             {
                 original.Next();
@@ -25,16 +27,32 @@ namespace Codestellation.Quarks.Tests
                 expected[i] = original.Next();
             }
 
-            var restored = new Random();
+            var restored = new Random(0);
             restored.RestoreState(state);
 
             var actual = new int[n];
             for (var i = 0; i < actual.Length; i++)
             {
                 actual[i] = restored.Next();
-            }                        
+            }
 
             Assert.That(actual, Is.EquivalentTo(expected));
         }
+#if NET6_0_OR_GREATER
+
+        [TestCaseSource(nameof(CreateIncompatibleRandoms))]
+        public void Should_throw_for_incompatible_implementations_on_get_state(Random random)
+            => Assert.Throws<ArgumentException>(() => random.GetState());
+
+        [TestCaseSource(nameof(CreateIncompatibleRandoms))]
+        public void Should_throw_for_incompatible_implementations_on_restore_state(Random random)
+            => Assert.Throws<ArgumentException>(() => random.RestoreState(Array.Empty<byte>()));
+
+        public static IEnumerable<Random> CreateIncompatibleRandoms()
+        {
+            yield return new Random();
+            yield return Random.Shared;
+        }
+#endif
     }
 }
